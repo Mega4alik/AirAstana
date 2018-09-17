fcc8b733373d5482624d711322a6603e14538baa
raushan <raushanm.re@gmail.com>
2018-09-17 17:53:10 +0600

    dialogflow java api added

--- src/main/java/cm/API.java
+++ src/main/java/cm/API.java
@@ -30,6 +30,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
+// Imports the Google Cloud client library
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.QueryResult;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.TextInput;
+import com.google.cloud.dialogflow.v2.TextInput.Builder;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import dialogflow.DFNode;
+import java.util.Collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.jdesktop.swingx.util.OS;
 /**
  *
  * @author raushan
@@ -69,6 +86,131 @@ public class API {
     }
 
     
+
+  // [START dialogflow_detect_intent_text]
+  /**
+   * Returns the result of detect intent with texts as inputs.
+   *
+   * Using the same `session_id` between requests allows continuation of the conversation.
+   * @param projectId Project/Agent Id.
+   * @param texts The text intents to be detected based on what a user says.
+   * @param sessionId Identifier of the DetectIntent session.
+   * @param languageCode Language code of the query.
+   */
+  public static DFNode detectIntentTexts(DFNode node, String projectId, String text, String sessionId,
+      String languageCode) throws Exception {
+      
+    // Instantiates a client
+    try (SessionsClient sessionsClient = SessionsClient.create()) {
+      // Set the session name using the sessionId (UUID) and projectID (my-project-id)
+      SessionName session = SessionName.of(projectId, sessionId);
+      System.out.println("Session Path: " + session.toString());
+
+      // Detect intents for each text input
+        // Set the text (hello) and language code (en-US) for the query
+        Builder textInput = TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
+
+        // Build the query with the TextInput
+        QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+
+        // Performs the detect intent request
+        DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+
+        // Display the query result
+        QueryResult queryResult = response.getQueryResult();
+
+        System.out.println("====================");
+        System.out.format("Query Text: '%s'\n", queryResult.getQueryText());
+        System.out.format("Detected Intent: %s (confidence: %f)\n",
+            queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
+        System.out.format("Fulfillment Text: '%s'\n", queryResult.getFulfillmentText());
+        
+        node.speech = queryResult.getFulfillmentText();
+        String intentName = queryResult.getIntent().getDisplayName();
+            /*
+                if (fulfillment.has("messages")){
+                   String message = fulfillment.getJSONArray("messages").getJSONObject(0).getString("speech");
+                   System.out.println(message);
+            }*/
+            Boolean allRequiredParamsPresent = queryResult.getAllRequiredParamsPresent();
+            Struct l = queryResult.getParameters();
+            switch (intentName){
+                case ("through_registration"):{
+                    node.intentName = intentName;
+                    if (allRequiredParamsPresent){
+                        
+                            Value city_from = l.getFieldsOrDefault("city_from", null);
+                            Value city_to = l.getFieldsOrDefault("city_to", null);                        
+                            node.speech = "Информация о сквозной регистрации " + city_from.getStringValue() + "-" +city_to.getStringValue();
+
+                    }
+                    break;
+                }
+                case ("flight_time"):{
+                    node.intentName = intentName;
+                    if (allRequiredParamsPresent){
+                             Value city_from = l.getFieldsOrDefault("city_from", null);
+                             Value city_to = l.getFieldsOrDefault("city_to", null);                                   
+                             node.speech = "Время вылета " + city_from.getStringValue() + "-" +city_to.getStringValue();
+                        }
+                    
+                    break;
+                }
+                case ("rebook_ticket"):{
+                    node.intentName = intentName;
+                    if (allRequiredParamsPresent){
+                            Value ticketNumber = l.getFieldsOrDefault("ticket_numer", null);                        
+                            node.speech = "Информация брони по перебронированию билета "+ ticketNumber.getStringValue();
+                        }
+                    
+                    break;
+                }
+                
+                case ("change ticket"):{
+                    node.intentName = "operator_connect";
+                    break;
+                }
+                case ("boardingpass_lost"):{
+                    node.intentName = intentName;
+                    break;
+                }
+                case ("baggage_allowed"):{
+                    node.intentName = intentName;
+                    Value baggage_allowed = l.getFieldsOrDefault("baggage_allowed", null);
+
+                    if (!baggage_allowed.getStringValue().isEmpty()){  
+
+                    } else {
+                        node.speech = "\"Предметы личного пользования разрешенные к перевозке :\n" +
+                                            "один предмет верхней одежды или плед;\n" +
+                                            "одна трость/пара костылей или скобы для ног, при условии, что пассажир не может без них обойтись;\n" +
+                                            "одну женскую сумочку или портфель/сумку для компьютера;\n" +
+                                            "один небольшой фотоаппарат или бинокль;\n" +
+                                            "небольшое количество материалов для чтения во время полета;\n" +
+                                            "одну переносную детскую кроватку;\n" +
+                                            "одну складную детскую коляску (максимальный размер в свернутом состоянии 34х32х14см);\n" +
+                                            "детское питание для употребления во время полета;\n" +
+                                            "медикаменты, без которых пассажир не может обойтись;\n" +
+                                            "маленький букет цветов.\"";
+                    }
+                    break;
+                }
+                default: break;
+
+            }
+        }
+        
+        return node;
+  }
+    
+    
+
+
+
+  public static void main(String[] args) throws Exception{
+        
+    }
 }
+    
 
 
